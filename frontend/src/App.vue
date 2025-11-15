4018ef0c37338abc4e2544d3276a320d37a089b1
ON24-MK <michelle_kleindienst@yahoo.com>
2025-11-15 16:48:39 +0100

    testen von medi anlegen

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -9,12 +9,21 @@
 
     <hr style="margin: 2rem 0;" />
 
-    <h2>Hier kommt später deine App:</h2>
+    <h2>App:</h2>
     <ul>
       <li>Medikamentenliste</li>
       <li>Medikament hinzufügen</li>
       <li>Tagesübersicht</li>
       <li>Tagebuch</li>
+    </ul>
+
+    <hr style="margin: 2rem 0;" />
+
+    <h2>Medikamentenliste (aus Backend):</h2>
+    <ul>
+      <li v-for="med in medications" :key="med.id">
+        {{ med.name }} – {{ med.dosage }} ({{ med.times.join(", ") }})
+      </li>
     </ul>
   </div>
 </template>
@@ -22,13 +31,22 @@
 <script setup>
 import { ref, onMounted } from 'vue';
 
-// Backend-Antwort
+// Backend
 const health = ref(null);
+
+// Medikamente
+const medications = ref([]);
 
 onMounted(async () => {
   try {
     const res = await fetch("http://localhost:8000/api/health");
     health.value = await res.json();
+
+    // die Medis laden
+    const medsRes = await fetch("http://localhost:8000/api/medications");
+    const medsData = await medsRes.json();
+    medications.value = medsData.medications;
+
   } catch (err) {
     console.error("Fehler beim Fetch:", err);
     health.value = { error: "Backend nicht erreichbar" };
