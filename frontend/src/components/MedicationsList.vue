93a38db6692f9bc7906969f4d3bec692bd6d2613
ON24-MK <michelle_kleindienst@yahoo.com>
2025-11-16 14:12:09 +0100

    weitere komponente bauen

---
+++ frontend/src/components/MedicationsList.vue
@@ -0,0 +1,25 @@
+<script setup>
+const props = defineProps({
+  medications: {
+    type: Array,
+    default: () => [],
+  },
+});
+</script>
+
+<template>
+  <section>
+    <h2>Medikamentenliste (aus Backend):</h2>
+
+    <ul v-if="props.medications.length">
+      <li v-for="med in props.medications" :key="med.id">
+        {{ med.name }} – {{ med.dosage }}
+        <span v-if="med.times && med.times.length">
+          ({{ med.times.join(", ") }})
+        </span>
+      </li>
+    </ul>
+
+    <p v-else>Keine Medikamente geladen.</p>
+  </section>
+</template>
